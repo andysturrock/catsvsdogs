5d2d0ccb5a169a52af164fd707f97fba36738273
Andy Sturrock <github@sturrock.org>
2020-11-03 17:01:10 +0000

    Pretty much working

--- model_data.py
+++ model_data.py
@@ -6,10 +6,9 @@ import numpy as np
 from tqdm import tqdm
 
 class ModelData():
-    IMG_SIZE = 50
     CATS = "PetImages/Cat"
     DOGS = "PetImages/Dog"
-    TESTING = "PetImages/Testing"
+    # TESTING = "PetImages/Testing"
     LABELS = {CATS: 0, DOGS: 1}
     TRAINING_DATAFILE = "PetImages/training_data.npy"
     TESTING_DATAFILE = "PetImages/testing_data.npy"
@@ -20,44 +19,50 @@ class ModelData():
     testing_catcount = 0
     testing_dogcount = 0
 
-    def __init__(self):
-        if (not os.path.isfile(self.TRAINING_DATAFILE) or not os.path.isfile(self.TESTING_DATAFILE)):
-            for label in self.LABELS:
-                print(f"{label}\n")
-                for f in tqdm(os.listdir(label)):
-                    if "jpg" in f:
-                        try:
-                            path = os.path.join(label, f)
-                            img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-                            img = cv2.resize(img, (self.IMG_SIZE, self.IMG_SIZE))
-                            # Randomly take 10% of the images as testing data
-                            if(random.randint(0, 100) > 90):
-                                # Each element is a tensor with two elements:
-                                # elem[0] is the image
-                                # elem[1] is a "one-hot" tensor of the classification
-                                # "one-hot" means if the image is a cat elem[1] is [1., 0.] and
-                                # if the image is a dog then elem [1] is [0., 1.]
-                                self.testing_data.append([np.array(img), np.eye(2)[self.LABELS[label]]])
-                                if label == self.CATS:
-                                    self.testing_catcount += 1
-                                elif label == self.DOGS:
-                                    self.testing_dogcount += 1
-                            else:
-                                self.training_data.append([np.array(img), np.eye(2)[self.LABELS[label]]])
-                                if label == self.CATS:
-                                    self.training_catcount += 1
-                                elif label == self.DOGS:
-                                    self.training_dogcount += 1
-                        except Exception:
-                            pass
-        
-            np.random.shuffle(self.training_data)
-            np.save(self.TRAINING_DATAFILE, self.training_data)
-            np.save(self.TESTING_DATAFILE, self.testing_data)
-            print('Training Cats:', self.training_catcount)
-            print('Training Dogs:', self.training_dogcount)
-            print('Training Cats:', self.testing_catcount)
-            print('Training Dogs:', self.testing_dogcount)
+    def __init__(self, img_size):
+        # If both training and testing files exist already then just exit
+        if (os.path.isfile(self.TRAINING_DATAFILE) and os.path.isfile(self.TESTING_DATAFILE)):
+            return
+
+        # Use a "one-hot" tensor for the classification
+        # "one-hot" means if the image is a cat then the tensor is [1., 0.]
+        # and if the image is a dog then it's [0., 1.]
+        one_hot = {self.CATS: [1.0, 0.0], self.DOGS: [0.0, 1.0]}
+
+        for label in self.LABELS:
+            for f in tqdm(os.listdir(label)):
+                if "jpg" in f:
+                    path = os.path.join(label, f)
+                    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+                    if img is None: # imread returns None if it can't read the image so just ignore this one
+                        continue
+                    img = cv2.resize(img, (img_size, img_size))
+
+                    # Take a random 10% of the images as testing data
+                    if(random.randint(0, 100) > 90):
+                        # Each element is a tensor with two elements:
+                        # elem[0] is the image
+                        # elem[1] is a the classification as a one-hot vector
+                        self.testing_data.append([np.array(img), one_hot[label]])
+                        if label == self.CATS:
+                            self.testing_catcount += 1
+                        elif label == self.DOGS:
+                            self.testing_dogcount += 1
+                    else:
+                        self.training_data.append([np.array(img), one_hot[label]])
+                        if label == self.CATS:
+                            self.training_catcount += 1
+                        elif label == self.DOGS:
+                            self.training_dogcount += 1
+
+    
+        np.random.shuffle(self.training_data)
+        np.save(self.TRAINING_DATAFILE, self.training_data)
+        np.save(self.TESTING_DATAFILE, self.testing_data)
+        print('Training Cats:', self.training_catcount)
+        print('Training Dogs:', self.training_dogcount)
+        print('Testing Cats:', self.testing_catcount)
+        print('Testing Dogs:', self.testing_dogcount)
 
     def get_training_data(self):
         if(len(self.training_data) == 0):
