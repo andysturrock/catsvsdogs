ea3da267381139f41c701433bf8f56d32959b32b
Andy Sturrock <github@sturrock.org>
2020-11-03 14:04:15 +0000

    Pretty much working

--- cnn.py
+++ cnn.py
@@ -1,36 +1,52 @@
+import time
+
+from tqdm import tqdm
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
 class CNN(nn.Module):
-    def __init__(self, device):
+    def __init__(self, img_size, kernel_size, device):
         super().__init__()
+        self.img_size = img_size
         self.device = device
 
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        self.conv2 = nn.Conv2d(32, 64, 5)
-        self.conv3 = nn.Conv2d(64, 128, 5)
+        # Layer 1 takes in 1 channel and returns 32 channels, with "kernel" (like a window on the image) of kernel_size
+        self.conv1 = nn.Conv2d(1, 32, kernel_size)
+        # Layer 2 takes in 32 channels (from layer 1) and returns 64 channels, with kernel of kernel_size
+        self.conv2 = nn.Conv2d(32, 64, kernel_size)
+        # Layer 3 takes in 64 channels (from layer 2) and returns 128 channels, with kernel of kernel_size
+        self.conv3 = nn.Conv2d(64, 128, kernel_size)
 
-        x = torch.randn(50,50).view(-1,1,50,50)
+        # We need to work out the shape of the input features for the first Linear layer
+        # So stick some random data through the convolutional layers and use that to calculate it.
+        x = torch.randn(self.img_size, self.img_size).view(-1, 1, self.img_size, self.img_size)
         self._to_linear = None
         self.convs(x)
 
+        # So now linear layer 1 takes in size "_to_linear" input sample and returns output sample size 512
         self.fc1 = nn.Linear(self._to_linear, 512)
+        # Linear layer 2 takes the sample size 512 and cuts it down to 2.
         self.fc2 = nn.Linear(512, 2)
 
+        # Use the Adam algorithm for optimisation.  See docs.
         self.optimizer = optim.Adam(self.parameters(), lr=0.001)
+        # Use Mean Squares Error for the loss function
         self.loss_function = nn.MSELoss()
 
+    # Run the convolutional layers
     def convs(self, x):
         x = F.max_pool2d(F.relu(self.conv1(x)), (2,2))
         x = F.max_pool2d(F.relu(self.conv2(x)), (2,2))
         x = F.max_pool2d(F.relu(self.conv3(x)), (2,2))
 
         if self._to_linear is None:
-            self._to_linear = x[0].shape[0]*x[0].shape[1]*x[0].shape[2]
+            self._to_linear = x[0].shape[0] * x[0].shape[1] * x[0].shape[2]
         return x
 
+    # Do the computation.  Inherited from base class.
     def forward(self, x):
         x = self.convs(x)
         x = x.view(-1, self._to_linear)
@@ -38,33 +54,40 @@ class CNN(nn.Module):
         x = self.fc2(x)
         return F.softmax(x, dim=1)
 
-    def train(self, epochs, batch_size, train_X, train_y):
-        for epoch in range(epochs):
-            for i in range(0, len(train_X), batch_size):
-                batch_X = train_X[i:i + batch_size].view(-1, 1, 50, 50)
-                batch_y = train_y[i:i + batch_size]
+    # Train the model
+    # train_X is the input images
+    # train_y is the input image classification in one-hot format: [1, 0] for cat, [0, 1] for dog
+    # logfile should be already opened for append.  Can be a file, or STDIN etc.
+    # Epoch is optional and can be used to show in the logfile how many times this function has been called.
+    def train(self, batch_size, train_X, train_y, logfile, epoch=1):
+        for i in tqdm(range(0, len(train_X), batch_size)):
+            batch_X = train_X[i:i + batch_size].view(-1, 1, self.img_size, self.img_size)
+            batch_y = train_y[i:i + batch_size]
 
-                batch_X, batch_y = batch_X.to(self.device), batch_y.to(self.device)
+            batch_X, batch_y = batch_X.to(self.device), batch_y.to(self.device)
 
-                self.zero_grad()
-                outputs = self(batch_X)
-                loss = self.loss_function(outputs, batch_y)
-                loss.backward()
-                self.optimizer.step()
+            self.zero_grad()
+            outputs = self(batch_X)
+            loss = self.loss_function(outputs, batch_y)
+            loss.backward()
+            self.optimizer.step()
 
-                matches = [torch.argmax(i) == torch.argmax(j) for i, j in zip(outputs, y)]
-                acc = matches.count(True)/len(matches)
-                loss = self.loss_function(outputs, y)
+            matches = [torch.argmax(i) == torch.argmax(j) for i, j in zip(outputs, batch_y)]
+            accuracy = matches.count(True)/len(matches)
 
-                print(f"Acc: {round(float(acc),2)}  Loss: {round(float(loss),4)}")
+            logfile.write(f"{int(time.time())},{epoch},in_sample,{round(float(accuracy),2)},{round(float(loss),4)}\n")
     
+    # Test the model.
+    # train_X is the input images
+    # train_y is the input image classification in one-hot format: [1, 0] for cat, [0, 1] for dog
+    # This data should be "out of sample" (ie not the same images used in training)
     def test(self, test_X, test_y):
         correct = 0
         total = 0
         with torch.no_grad():
             for i in range(len(test_X)):
                 real_class = torch.argmax(test_y[i]).to(self.device)
-                net_out = self(test_X[i].view(-1, 1, 50, 50).to(self.device))[0]
+                net_out = self(test_X[i].view(-1, 1, self.img_size, self.img_size).to(self.device))[0]
 
                 predicted_class = torch.argmax(net_out)
                 if predicted_class == real_class:
--- main.py
+++ main.py
@@ -1,29 +1,55 @@
+import datetime
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from cnn import CNN
+import matplotlib.pyplot as plt
 
 from model_data import ModelData
-from cnn import CNN
 
 
+IMG_SIZE = 50
+KERNEL_SIZE = 5
+BATCH_SIZE = 100
+EPOCHS = 5
 
 if torch.cuda.is_available():
-    device = torch.device("cuda:0")  # you can continue going on here, like cuda:1 cuda:2....etc. 
+    device = torch.device("cuda:0")
     print("Running on the GPU")
 else:
     device = torch.device("cpu")
     print("Running on the CPU")
 
-model_data = ModelData()
+print("Loading training and test data...")
+model_data = ModelData(IMG_SIZE)
 training_data = model_data.get_training_data()
+testing_data = model_data.get_testing_data()
 
-cnn = CNN(device)
-batch_size = 100
-epochs = 3
+print("Scaling training data...")
+training_images = torch.Tensor([i[0] for i in training_data]).view(-1, IMG_SIZE, IMG_SIZE)
+# The images are greyscale values between 0 and 255, whereas we need them
+# to be between 0 and 1 (as floats) so scale them.
+training_images = training_images / 255.0
+training_classifications = torch.Tensor([i[1] for i in training_data])
 
-X = torch.Tensor([i[0] for i in training_data]).view(-1, 50, 50)
-X = X/255.0
-y = torch.Tensor([i[1] for i in training_data])
+print("Scaling testing data...")
+testing_images = torch.Tensor([i[0] for i in testing_data]).view(-1, IMG_SIZE, IMG_SIZE)
+# See above
+testing_images = testing_images / 255.0
+testing_classifications = torch.Tensor([i[1] for i in testing_data])
 
-cnn.train(epochs, batch_size, torch.Tensor([i[0] for i in training_data]).view(-1, 50, 50), torch.Tensor([i[1] for i in training_data]))
+print("Creating neural net...")
+cnn = CNN(IMG_SIZE, KERNEL_SIZE, device)
+
+print("Training model...")
+now = datetime.datetime.now()
+nowStr = now.strftime("%Y-%m-%d_%H.%M.%S")
+MODEL_NAME = f"model-{nowStr}"
+with open(f"{MODEL_NAME}.csv", "a") as logfile:
+    # header row
+    logfile.write(f"timestamp,epoch,in_sample,accuracy,loss\n")
+    for epoch in range(EPOCHS):
+        cnn.train(BATCH_SIZE, training_images, training_classifications, logfile, epoch)
+        cnn.test(testing_images, testing_classifications)
