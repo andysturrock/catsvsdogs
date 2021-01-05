7f1cf91ba8f0585ce94035e728462f1f659898c0
Andy Sturrock <github@sturrock.org>
2021-01-05 18:42:38 +0000

    Rename files to TS naming standard

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkStack } from '../lib/cdk-stack';
+import { CatsVsDogsStack } from '../lib/catsVsDogsStack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
+new CatsVsDogsStack(app, 'CdkStack', {
     env: {
         region: 'eu-west-2',
         account: '089953642441',
--- cdk/lib/catsVsDogsStack.ts
+++ cdk/lib/catsVsDogsStack.ts
@@ -1,8 +1,8 @@
 import * as cdk from '@aws-cdk/core';
-import * as catsvsdogs_service from './catsvsdogs_service';
+import * as catsvsdogs_service from './catsVsDogsService';
 
 
-export class CdkStack extends cdk.Stack {
+export class CatsVsDogsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
